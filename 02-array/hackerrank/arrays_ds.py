6ce2a934acf930f1b4d569154f979a10e2a90ade
Arati Sankaliya <aratisankaliya12@gmail.com>
2026-01-08 06:20:23 +0530

    Update arrays_ds.py

--- 02-array/hackerrank/arrays_ds.py
+++ 02-array/hackerrank/arrays_ds.py
@@ -3,11 +3,28 @@ HackerRank - Arrays - DS
 Difficulty: Easy
 Link: https://www.hackerrank.com/challenges/arrays-ds/problem
 
+Approach:
+1. Use two pointers (left and right)
+2. Swap elements while left < right
+3. Reverse the array in-place
+4. Return the reversed array
+
+Key Learning:
+- Two-pointer technique
+- In-place array reversal
+- Index-based manipulation
+
 Time Complexity: O(n)
 Space Complexity: O(1)
 """
 
-n = int(input())
-arr = list(map(int, input().split()))
+def reverseArray(a):
+    l = 0
+    r = len(a) - 1
 
-print(*arr[::-1])
+    while l < r:
+        a[l], a[r] = a[r], a[l]
+        l += 1
+        r -= 1
+
+    return a
