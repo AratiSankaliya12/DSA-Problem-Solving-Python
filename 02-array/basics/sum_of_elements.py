d009ee329ba5a0d7e08ea4ba15af87b1a9463e03
Arati Sankaliya <aratisankaliya12@gmail.com>
2026-01-08 06:14:15 +0530

    Create sum_of_elements.py

---
+++ 02-array/basics/sum_of_elements.py
@@ -0,0 +1,16 @@
+"""
+Problem: Sum of array elements
+Difficulty: Easy
+Topic: Arrays (Basics)
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+arr = [1, 2, 3, 4]
+total = 0
+
+for num in arr:
+    total += num
+
+print(total)
