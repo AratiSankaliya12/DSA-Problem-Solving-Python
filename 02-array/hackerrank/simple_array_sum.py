6cde6789c6c1eccbc809f9c4b890ed7222a22715
Arati Sankaliya <aratisankaliya12@gmail.com>
2026-01-08 06:17:27 +0530

    Create simple_array_sum.py

---
+++ 02-array/hackerrank/simple_array_sum.py
@@ -0,0 +1,13 @@
+"""
+HackerRank - Simple Array Sum
+Difficulty: Easy
+Link: https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+print(sum(arr))
