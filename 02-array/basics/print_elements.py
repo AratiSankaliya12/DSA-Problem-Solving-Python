031a8062951cd3bcb067696c7911905f5583492f
Arati Sankaliya <aratisankaliya12@gmail.com>
2026-01-08 06:13:28 +0530

    Create print_elements.py

---
+++ 02-array/basics/print_elements.py
@@ -0,0 +1,13 @@
+"""
+Problem: Print all elements of an array
+Difficulty: Easy
+Topic: Arrays (Basics)
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+arr = [1, 2, 3, 4]
+
+for num in arr:
+    print(num)
